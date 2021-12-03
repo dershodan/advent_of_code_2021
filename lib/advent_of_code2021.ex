72a2e6e08bfd211f9780a4ab945382dd58afc006
Christoph Leitner <dershodan1@gmail.com>
2021-12-03 10:57:12 +0100

    initial

---
+++ lib/advent_of_code2021.ex
@@ -0,0 +1,18 @@
+defmodule AdventOfCode2021 do
+  @moduledoc """
+  Documentation for `AdventOfCode2021`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCode2021.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/advent_of_code2021_test.exs
@@ -0,0 +1,8 @@
+defmodule AdventOfCode2021Test do
+  use ExUnit.Case
+  doctest AdventOfCode2021
+
+  test "greets the world" do
+    assert AdventOfCode2021.hello() == :world
+  end
+end
