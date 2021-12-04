bf67e1122db36009cf457792dac957af02c48dd1
Christoph Leitner <dershodan1@gmail.com>
2021-12-04 13:26:55 +0100

    Add tests and implement second part of Part01

--- lib/part_01.ex
+++ lib/part_01.ex
@@ -1,5 +1,25 @@
 defmodule AOC2021.Part01 do
-  def larger_count(list, last_element, count) do
+  def count_increasing_values(list) do
+    larger_count(list, List.first(list))
+  end
+
+  def span_list(input) do
+    {_, _, summed_list} = Enum.reduce(input, {0, 0, []}, fn val, acc ->
+      case acc do
+        {0, n_minus_1, []} ->
+          {n_minus_1, val, []}
+        {0, 0, []} ->
+          {0, val, []}
+        {n_minus_2, n_minus_1, spanned_sum_list} ->
+          sum = n_minus_1 + n_minus_2 + val
+          spanned_sum_list = [sum | spanned_sum_list]
+          {n_minus_1, val, spanned_sum_list}
+      end
+    end)
+    Enum.reverse(summed_list)
+  end
+
+  def larger_count(list, last_element, count \\ 0) do
     case list do
       [h | t] when h > last_element ->
         larger_count(t, h, count + 1)
--- test/advent_of_code2021_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule AdventOfCode2021Test do
-  use ExUnit.Case
-  doctest AdventOfCode2021
-
-  test "greets the world" do
-    assert AdventOfCode2021.hello() == :world
-  end
-end
